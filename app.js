84f3769f60c95dc6ab4d4c62480a2a5995374332
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-06-30 16:18:21 -0400

    Updated the function to include the new parameter.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const http = new simpleHTTP();
 
 // GET posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(response) {
+http.get('https://jsonplaceholder.typicode.com/posts', function(
+  error,
+  response
+) {
   console.log(response);
 });
