01edbdd6b690918be5b0f958fce55155e87f675c
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-06-30 15:46:24 -0400

    Added the HTTP GET protoype.

--- simplehttp.js
+++ simplehttp.js
@@ -1,3 +1,11 @@
 function easyHTTP() {
-    this.http = new XMLHttpRequest();
+  this.http = new XMLHttpRequest();
 }
+
+// HTTP GET request
+easyHTTP.prototype.get = function() {};
+// HTTP POST request
+
+// HTTP PUT request
+
+//HTTP Delete request
